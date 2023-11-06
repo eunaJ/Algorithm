1fc5e9397d1aae708e8fe0369f70fb084cc37955
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2023-11-06 12:27:40 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 2.48 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        for(int i=0; i<queries.length; i++){
+            int min = 1000000;
+            for(int j=queries[i][0]; j<=queries[i][1]; j++){
+                if(queries[i][2] < arr[j]){
+                    if (min > arr[j]){
+                        min = arr[j];
+                    }
+                }
+            }
+            if (min == 1000000) answer[i] = -1;
+            else answer[i] = min;
+        }
+        return answer;
+    }
+}
