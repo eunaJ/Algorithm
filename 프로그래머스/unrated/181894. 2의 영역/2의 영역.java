419bb1153d6e0be78298c0526f56197c681fd057
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-01-05 10:49:51 +0900

    [level unrated] Title: 2의 영역, Time: 2.32 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] arr) {
+        int s = -1;
+        int e = -1;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == 2){
+                if(s == -1) {s = i; e = i;}
+                else{e = i;}
+            }  
+        }
+        if(s == -1 && e == -1) return new int[]{-1};
+        else{
+            return Arrays.copyOfRange(arr, s, e+1);
+        }
+    }
+}
