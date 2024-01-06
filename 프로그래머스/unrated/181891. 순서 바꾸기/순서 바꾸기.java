9c15a97b4664a06b49fad091abd04e62f323a2bb
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-01-06 09:55:45 +0900

    [level unrated] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        int j = 0;
+        for(int i=n; i<num_list.length; i++){
+            answer[j] = num_list[i];
+            j++;
+        }
+        for(int i=0; i<n; i++){
+            answer[j] = num_list[i];
+            j++;
+        }
+        return answer;
+    }
+}
