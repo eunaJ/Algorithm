cb13192e1f634b1686feed806f7a02c0436a1738
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-01-13 10:48:01 +0900

    [level Lv.0] Title: 정사각형으로 만들기, Time: 0.02 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/Lv.0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -22,3 +22,11 @@ class Solution {
         return answer;
     }
 }
+
+//
+// int max = Math.max(arr.length, arr[0].length);
+// int[][] array = new int[max][max];
+// for (int i = 0; i < arr.length; i++) {
+//     System.arraycopy(arr[i], 0, array[i], 0, arr[0].length);
+// }
+// return array;
