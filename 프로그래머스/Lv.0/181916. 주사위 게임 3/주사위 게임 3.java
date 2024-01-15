a8d781c623a6ab8d20085e9e6e15111d7b4e5cb4
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-01-15 09:40:45 +0900

    [level Lv.0] Title: 주사위 게임 3, Time: 0.36 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/Lv.0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -34,3 +34,18 @@ class Solution {
         }
     }
 }
+
+// 좀 더 깔끔
+// else if (dice[0] == dice[2] || dice[1] == dice[3]) {
+//     ans = (int) Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]), 2);
+// } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
+//     ans = (dice[0] + dice[3]) * (dice[3] - dice[0]);
+// } else if (dice[0] == dice[1]) {
+//     ans = dice[2] * dice[3];
+// } else if (dice[1] == dice[2]) {
+//     ans = dice[0] * dice[3];
+// } else if (dice[2] == dice[3]) {
+//     ans = dice[0] * dice[1];
+// } else {
+//     ans = dice[0];
+// }
