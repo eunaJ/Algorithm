2e240e9abc73258f8619239bbce569a11125438d
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-01-18 11:09:53 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -2,9 +2,12 @@ import java.lang.Math;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i=1; i<=n; i++){
+        if(n == 1) return 1;
+        if(n == 2) return 3;
+        for(int i=1; i<=Math.sqrt(n); i++){
             if(n % i == 0){
-                answer += i;
+                if(i == Math.sqrt(n)) answer += i;
+                else answer = answer + i + (n/i);
             }
         }
         return answer;
