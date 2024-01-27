433d482c51a51ab8ac293f000dc8fc1cee186f18
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-01-27 11:53:26 +0900

    Update 최소직사각형.java

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -13,3 +13,11 @@ class Solution {
         return answer*max;
     }
 }
+
+//
+// int length = 0, height = 0;
+// for (int[] card : sizes) {
+//     length = Math.max(length, Math.max(card[0], card[1]));
+//     height = Math.max(height, Math.min(card[0], card[1]));
+// }
+// int answer = length * height;
