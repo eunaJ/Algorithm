71d97ce5476592d76cd69220204351e735666ee9
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-01-31 10:37:18 +0900

    Update 완주하지 못한 선수.java

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -10,3 +10,14 @@ class Solution {
         return participant[participant.length-1];
     }
 }
+
+//
+// HashMap<String, Integer> hm = new HashMap<>();
+// for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+// for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+// for (String key : hm.keySet()) {
+//     if (hm.get(key) != 0){
+//         answer = key;
+//     }
+// }
