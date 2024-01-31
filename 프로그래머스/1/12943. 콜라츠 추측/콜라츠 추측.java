63ec104ecc7e2ea9e926cfe9f58ad1b103d6cfc9
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-01-31 10:24:29 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = num;
+        if(num == 1) return 0;
+        while(n > 1){
+            if(n % 2 == 0) n = n / 2;
+            else n = 3 * n + 1;
+            answer++;
+            if(answer >= 500) return -1;
+        }
+        return answer;
+    }
+}
