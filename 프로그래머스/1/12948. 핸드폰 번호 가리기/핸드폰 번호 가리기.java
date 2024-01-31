14d0c0a6d534ba7e6492cc982606c76192a0a52f
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-01-31 10:24:42 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,11 @@
+import java.lang.StringBuilder;
+class Solution {
+    public String solution(String phone_number) {
+        StringBuilder answer = new StringBuilder(phone_number);
+        int l = phone_number.length() - 4;
+        for(int i=0; i<l; i++){
+            answer = answer.replace(i, i+1, "*");
+        }
+        return String.valueOf(answer);
+    }
+}
