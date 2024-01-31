44e9aca247b06d976dad3b5ee469e414a002925b
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-01-31 10:36:03 +0900

    Update 햄버거 만들기.java

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -17,3 +17,18 @@ class Solution {
         return answer;
     }
 }
+
+//
+// int[] stack = new int[ingredient.length];
+// int sp = 0;
+// int answer = 0;
+// for (int i : ingredient) {
+//     stack[sp++] = i;
+//     if (sp >= 4 && stack[sp - 1] == 1
+// 	&& stack[sp - 2] == 3
+// 	&& stack[sp - 3] == 2
+// 	&& stack[sp - 4] == 1) {
+// 	sp -= 4;
+// 	answer++;
+//     }
+// }
