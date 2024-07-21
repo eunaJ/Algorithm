42b160c3e3f840f721f9b6c1a9b1e42118615418
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-07-21 13:50:55 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,1 @@
+select member_id, member_name, gender, date_format(date_of_birth, '%Y-%m-%d') date_of_birth from member_profile where gender='w' and month(date_of_birth)=3 and tlno is not null order by member_id;
