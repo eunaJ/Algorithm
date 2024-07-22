2df39ce79d7cb893de9583d7374ae39524e4edad
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-07-22 10:09:45 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,1 @@
+select book_id, author_name, date_format(published_date, '%Y-%m-%d') published_date from book join author on book.author_id = author.author_id where category = '경제' order by published_date;
