f95a46b0f1acf9b3f11d6983bf75f4408709dc95
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-07-23 09:28:37 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,1 @@
+select animal_ins.animal_id animal_id, animal_ins.name name from animal_ins join animal_outs on animal_ins.animal_id = animal_outs.animal_id where animal_ins.datetime > animal_outs.datetime order by animal_ins.datetime;
