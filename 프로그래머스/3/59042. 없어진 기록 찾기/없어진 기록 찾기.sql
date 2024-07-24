d122d26ad627d1b5750f1e7769bac3d5753fe9a3
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-07-24 10:52:02 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,4 @@
+select animal_outs.animal_id animal_id, animal_outs.name name
+from animal_ins right join animal_outs on animal_ins.animal_id = animal_outs.animal_id
+where animal_ins.animal_id is null
+order by animal_id;
