a9cbaffabae371831f6234e328a30eda539cb9fa
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-08-02 18:38:55 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,4 @@
+select i.item_id, item_name, rarity 
+from item_info i join item_tree t on i.item_id = t.item_id 
+where t.parent_item_id in (select item_id from item_info where rarity = 'RARE')
+order by i.item_id desc;
