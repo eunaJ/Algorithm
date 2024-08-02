2fa8bb8b2ca9e190c8680a88f1610bf9dbc68669
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-08-02 18:54:31 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,16 @@
+select e.emp_no emp_no, emp_name, 
+    case 
+        when avg(score) >= 96 then 'S'
+        when avg(score) >= 90 then 'A'
+        when avg(score) >= 80 then 'B'
+        else 'C'
+    end as grade,
+    case
+        when avg(score) >= 96 then sal * 0.2
+        when avg(score) >= 90 then sal * 0.15
+        when avg(score) >= 80 then sal * 0.1
+        else 0
+    end as bonus
+from hr_employees e join hr_grade g on e.emp_no = g.emp_no
+group by e.emp_no
+order by e.emp_no;
