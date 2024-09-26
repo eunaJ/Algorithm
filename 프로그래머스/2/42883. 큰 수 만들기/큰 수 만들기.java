4af418ba50d5348d79da36a0659e687134a0d4de
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-09-26 20:10:53 +0900

    [level 2] Title: 큰 수 만들기, Time: 7148.49 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        int n = 0;
+        for(int i=0; i<number.length()-k; i++) {
+            char m = '0';
+            for(int j=n; j<=k+i; j++){ 
+                if(number.charAt(j) > m){
+                    m = number.charAt(j);
+                    n = j + 1;
+                }
+            }
+            answer.append(m);
+        }
+        return answer.toString();
+    }
+}
